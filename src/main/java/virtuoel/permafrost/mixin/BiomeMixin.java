0d6c3e1f654f67a06f7d738896f1af5b3488c500
Virtuoel <Virtuoel@users.noreply.github.com>
2020-01-01 20:08:54 +0300

    Update mappings

--- src/main/java/virtuoel/permafrost/mixin/BiomeMixin.java
+++ src/main/java/virtuoel/permafrost/mixin/BiomeMixin.java
@@ -6,21 +6,21 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.LightType;
-import net.minecraft.world.ViewableWorld;
+import net.minecraft.world.WorldView;
 import net.minecraft.world.biome.Biome;
 import virtuoel.permafrost.api.PermafrostConfig;
 
 @Mixin(Biome.class)
 public abstract class BiomeMixin
 {
-	@Redirect(method = "canSetIce(Lnet/minecraft/world/ViewableWorld;Lnet/minecraft/util/math/BlockPos;)Z", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/ViewableWorld;getLightLevel(Lnet/minecraft/world/LightType;Lnet/minecraft/util/math/BlockPos;)I"))
-	private int canSetIceGetLightLevelProxy(ViewableWorld obj, LightType var1, BlockPos var2)
+	@Redirect(method = "canSetIce(Lnet/minecraft/world/WorldView;Lnet/minecraft/util/math/BlockPos;)Z", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/ViewableWorld;getLightLevel(Lnet/minecraft/world/LightType;Lnet/minecraft/util/math/BlockPos;)I"))
+	private int canSetIceGetLightLevelProxy(WorldView obj, LightType var1, BlockPos var2)
 	{
 		return PermafrostConfig.DATA.get("iceFormingInLight").getAsBoolean() ? 0 : obj.getLightLevel(var1, var2);
 	}
 	
-	@Redirect(method = "canSetSnow(Lnet/minecraft/world/ViewableWorld;Lnet/minecraft/util/math/BlockPos;Z)Z", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/ViewableWorld;getLightLevel(Lnet/minecraft/world/LightType;Lnet/minecraft/util/math/BlockPos;)I"))
-	private int canSetSnowGetLightLevelProxy(ViewableWorld obj, LightType var1, BlockPos var2)
+	@Redirect(method = "canSetSnow(Lnet/minecraft/world/WorldView;Lnet/minecraft/util/math/BlockPos;Z)Z", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/ViewableWorld;getLightLevel(Lnet/minecraft/world/LightType;Lnet/minecraft/util/math/BlockPos;)I"))
+	private int canSetSnowGetLightLevelProxy(WorldView obj, LightType var1, BlockPos var2)
 	{
 		return PermafrostConfig.DATA.get("snowLayerFormingInLight").getAsBoolean() ? 0 : obj.getLightLevel(var1, var2);
 	}
